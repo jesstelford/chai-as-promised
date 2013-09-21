93f5c538aef769f46506fb2169a57cdb321edd2e
domenic <domenic@domenicdenicola.com>
2013-09-21 18:17:46 -0400

    Better error message for bad reasons.

--- test/support/common.js
+++ test/support/common.js
@@ -23,7 +23,7 @@ global.shouldFail = function (options) {
         promiseProducer().then(function () {
             done(new Error("Expected promise to be rejected with an assertion error, but it was fulfilled"));
         }, function (reason) {
-            if (reason.constructor.name !== "AssertionError") {
+            if (Object(reason) !== reason || reason.constructor.name !== "AssertionError") {
                 done(new Error("Expected promise to be rejected with an AssertionError but it was rejected with " +
                                reason));
             } else {
